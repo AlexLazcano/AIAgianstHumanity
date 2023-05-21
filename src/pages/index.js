4e4143a25265185f6d62e49e758a34e701859a78
Jason Leung <jasonleung14@gmail.com>
2023-05-20 19:03:44 -0700

    Implemented pulling data from server

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,23 @@
 import Card from '@/components/card/card';
 import { io } from 'socket.io-client';
+import { useEffect } from "react";
 
 const Index = () => {
   
   const socket = io('ws://localhost:3001');
   // console.log(socket);
 
-  socket.on("connect", () => {
-    console.log("Successfully connected");
-  })
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Successfully connected");
+    });
+
+    socket.emit("getBlackCard", null);
+
+    socket.on("blackCards", (msg) => {
+      console.log(msg);
+    })
+  });
 
   return (
     <div>
