d45f59228f083da693bd7cd83d0713cc55647657
Alex Lazcano <alazcano2000@gmail.com>
2023-05-20 23:59:44 -0700

    card ui with socket

---
+++ src/components/cardList/style.js
@@ -0,0 +1,12 @@
+const { styled } = require("styled-components");
+
+
+const StyleCardList = styled.div`
+width: 100%;
+border: 2px solid black;
+height: 300px;
+display: flex;
+  
+`;
+
+export { StyleCardList }
