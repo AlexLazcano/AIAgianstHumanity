2c76187c6c02172c284deb43399b0ad92fb4a467
NickMoo5 <nicholasamah@gmail.com>
2023-05-20 20:13:27 -0700

    header, join game button

---
+++ src/components/header/styledInputBox.js
@@ -0,0 +1,12 @@
+const { styled } = require("styled-components");
+
+export const StyledInputBox = styled.input`
+    background-color:#c5c9c6;
+    padding: 5px;
+    font-size: 25px;
+    text-align: center;
+    font: 'Roboto', sans-serif;
+    color: black;
+    border-radius: 5px;
+    width: 150px;
+`
