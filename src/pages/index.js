97a21b515ae2a63a310344afa711cdedc541e949
Alex Lazcano <alazcano2000@gmail.com>
2023-05-20 17:01:02 -0700

    socket test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,25 @@
-import Card from '@/components/card/card'
-import React from 'react'
+import Card from '@/components/card/card';
+import { useSocket } from '@/lib/socket';
+import { useEffect } from 'react';
 
 const Index = () => {
+  const socket = useSocket('http://localhost:3000'); // Provide the URL of your Socket.io server
+  useEffect(() => {
+    // Add event listeners or emit events using the socket object
+    socket.on('connect', () => {
+      console.log('Connected to Socket.io server');
+    });
+
+    return () => {
+      // Clean up event listeners if necessary
+      socket.off('connect');
+    };
+  }, [socket]);
+
+
   return (
     <div>
-        <Card />
+      <Card />
     </div>
   )
 }
